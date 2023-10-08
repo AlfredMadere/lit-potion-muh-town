0bacaa1dd59e29b52e95ce1439e41afca4b6a2c2
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2023-10-08 15:31:05 -0700

    fixed sku malformatting and inventory usage bug

--- src/models/tests/test_transaction.py
+++ src/models/tests/test_transaction.py
@@ -2,5 +2,7 @@ import logging
 from ..transaction import Transaction
 def test_get_current_balance():
   current_balance = Transaction.get_current_balance()
-  assert current_balance == 0
-  
+  assert current_balance == 1500
+
+
+
