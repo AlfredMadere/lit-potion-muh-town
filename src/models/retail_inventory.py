6c92107fcc8d4fe4cea581b37792af43f42a74bd
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2023-10-09 10:13:18 -0700

    selling potions for 1 dollar

--- src/models/retail_inventory.py
+++ src/models/retail_inventory.py
@@ -13,7 +13,7 @@ class PotionInventory(BaseModel):
 
 class RetailInventory:
   table_name = "retail_inventory"
-  potion_price = 30 
+  potion_price = 1 
   def __init__(self, id, sku: str, name: str, type, quantity, price):
     self.id = id
     self.sku = sku
