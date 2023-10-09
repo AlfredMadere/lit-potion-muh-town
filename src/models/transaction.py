995409cd6ca8ced1f139d38450aeec32737e70db
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2023-10-09 13:45:39 -0700

    basic tests for new transactions and invoices strategy

--- src/models/transaction.py
+++ src/models/transaction.py
@@ -4,41 +4,36 @@ class Transaction:
   table_name = "transaction"
 
   #TODO: update template code
-  def __init__(self, id, starting_balance, ending_balance, transaction_entity_id, created_at):
+  def __init__(self, id, debit_credit: int, invoice_id, created_at):
     self.id = id
-    self.endin_balance = ending_balance
-    self.starting_balance = starting_balance
-    self.transaction_entity_id = transaction_entity_id
+    self.debit_credit = debit_credit
+    self.invoice_id = invoice_id
+
     self.created_at = created_at
 
   @staticmethod
   def get_current_balance():
+    #FIXME: update to create an invoice for initial transaction
     current_balance = 0
-    #get the most recent transaction and extract the ending balance, if there are no transactions then create the first transaction that is zeroed out
-    sql_to_execute = text(f"SELECT id, ending_balance FROM {Transaction.table_name} ORDER BY created_at DESC LIMIT 1")
+    sql_to_execute = text(f"SELECT id, debit_credit FROM {Transaction.table_name}")
     with db.engine.begin() as connection:
       result = connection.execute(sql_to_execute)
-      row = result.fetchone()
-      if row is None:
-        #FIXME: add a description to the OG transaction
-        sql_to_execute = text(f"INSERT INTO {Transaction.table_name} (starting_balance, ending_balance) VALUES (0, 100)")
+      rows = result.fetchall()
+      for row in rows:
+        current_balance += row[1]
+      if rows is None or rows == [] :
+        sql_to_execute = text(f"INSERT INTO {Transaction.table_name} (debit_credit) VALUES (100)")
         result = connection.execute(sql_to_execute)
         current_balance = 100
-      else:
-        current_balance = row[1]
-        print("id", row[0])
     return current_balance
 
 
   @staticmethod
-  def create(transaction_entity_id: int | None, adjustment: int, description: str ):
+  def create(debit_credit: int, invoice_id: int):
     try:
-      current_balance = Transaction.get_current_balance()
-      print("current balance: ", current_balance )
-      new_balance = current_balance + adjustment
-      sql_to_execute = text(f"INSERT INTO {Transaction.table_name} (starting_balance, ending_balance, transaction_entity_id, description) VALUES (:starting_balance, :ending_balance, :transaction_entity_id, :description)")
+      sql_to_execute = text(f"INSERT INTO {Transaction.table_name} (debit_credit, invoice_id) VALUES (:debit_credit, :invoice_id)")
       with db.engine.begin() as connection:
-        result = connection.execute(sql_to_execute, {"starting_balance": current_balance, "ending_balance": new_balance, "transaction_entity_id": transaction_entity_id, "description": description})
+        result = connection.execute(sql_to_execute, {"debit_credit": debit_credit, "invoice_id": invoice_id})
       return "OK"
     except Exception as error:
       print("unable to create transaction: ", error)
